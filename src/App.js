58eef9efb6bd655bd4f274c6e023824fc012e500
yurisena <ybsena@inf.ufpel.edu.br>
2021-04-25 19:55:10 -0300

    eslint, prettier and babel config done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Capt. Janeway';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
