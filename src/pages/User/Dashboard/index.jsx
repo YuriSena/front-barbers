5e175c282b9d86b713910bb2711ec18277291d4f
Rafael Ferreira <rafaetri@gmail.com>
2021-05-17 00:37:59 -0300

    added providerDashboard

---
+++ src/pages/User/Dashboard/index.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+// import { FaArrowCircleDown } from 'react-icons/fa';
+import { MainContainer } from './styles';
+import profileImageDefault from '../../../assets/profileImageDefault.PNG';
+
+const UserDashboard = () => {
+  const [open, setOpen] = useState(false);
+  const history = useHistory();
+
+  const handleProfileConfig = () => {
+    history.push('/user-dashboard/profile-config');
+  };
+
+  return (
+    <MainContainer>
+      <div id="menu-avatar">
+        <div
+          id="image-container"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          <img
+            id="profile-image"
+            src={profileImageDefault}
+            alt="profile-default"
+          />
+          <span id="user-name">Nome Completo</span>
+          {/* <FaArrowCircleDown id="menu-arrow" /> */}
+        </div>
+
+        {open ? (
+          <div id="pop-up-container">
+            <span id="profile-config" onClick={handleProfileConfig}>
+              Editar perfil
+            </span>
+            <span>Sair</span>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+
+      <div id="content-container">
+        <h1 id="content-title">Lista de barbeiros disponíveis</h1>
+        <div id="barber-container">
+          <img id="barber-image" src={profileImageDefault} alt="barber" />
+          <div id="barber-info-container">
+            <span>Vinicius Lemos</span>
+            <span>Rua dos bobos, 24</span>
+          </div>
+        </div>
+
+        <div id="barber-container">
+          <img id="barber-image" src={profileImageDefault} alt="barber" />
+          <div id="barber-info-container">
+            <span>Vinicius Lemos</span>
+            <span>Rua dos bobos, 24</span>
+          </div>
+        </div>
+
+        <div id="barber-container">
+          <img id="barber-image" src={profileImageDefault} alt="barber" />
+          <div id="barber-info-container">
+            <span>Vinicius Lemos</span>
+            <span>Rua dos bobos, 24</span>
+          </div>
+        </div>
+
+        <div id="barber-container">
+          <img id="barber-image" src={profileImageDefault} alt="barber" />
+          <div id="barber-info-container">
+            <span>Vinicius Lemos</span>
+            <span>Rua dos bobos, 24</span>
+          </div>
+        </div>
+
+        <div id="barber-container">
+          <img id="barber-image" src={profileImageDefault} alt="barber" />
+          <div id="barber-info-container">
+            <span>Vinicius Lemos</span>
+            <span>Rua dos bobos, 24</span>
+          </div>
+        </div>
+      </div>
+    </MainContainer>
+  );
+};
+
+export default UserDashboard;
---
+++ src/pages/User/Login/index.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+
+import { useHistory } from 'react-router-dom';
+
+import { MainContainer } from './styles';
+
+import barberIcon from '../../../assets/barberIcon1.PNG';
+import { colors } from '../../../colors';
+import { api } from '../../../api';
+
+const UserLogin = () => {
+  const history = useHistory();
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleLogin = () => {
+    const data = {
+      email: inputs.email,
+      password: inputs.password,
+    };
+    api.post('/clients/signin', data).then(() => {
+      history.push('/user-dashboard');
+      console.log('vinicio cocao');
+    });
+  };
+
+  return (
+    <MainContainer>
+      <div id="logo-container">
+        <img id="logo-image" src={barberIcon} alt="barber-icon" />
+        {/* <h1 id="logo-title">Barbers</h1> */}
+      </div>
+
+      <div id="modal-container">
+        <div id="modal-title-container">
+          <h1>Informe seus dados para o login</h1>
+          <h3>
+            Tipo de Login: <h3 style={{ color: colors.SkyBlue }}>Cliente</h3>
+          </h3>
+        </div>
+
+        <div id="form-container">
+          <label htmlFor="email">
+            Email
+            <input
+              placeholder="Email"
+              type="text"
+              id="email"
+              name="email"
+              value={inputs.email}
+              onChange={(e) => {
+                setInputs({ ...inputs, [e.target.name]: e.target.value });
+              }}
+            />
+          </label>
+
+          <label htmlFor="password">
+            Senha
+            <input
+              placeholder="Senha"
+              type="password"
+              id="password"
+              name="password"
+              value={inputs.password}
+              onChange={(e) => {
+                setInputs({ ...inputs, [e.target.name]: e.target.value });
+              }}
+            />
+          </label>
+
+          <div id="question-container">
+            <span>Não tem conta?</span>
+            <span
+              onClick={() => {
+                history.push('/user-register');
+              }}
+              id="link-to-register"
+            >
+              Cadastre-se aqui.
+            </span>
+          </div>
+        </div>
+
+        <div id="button-container">
+          <button
+            onClick={() => {
+              history.push('/provider-login');
+            }}
+            type="button"
+          >
+            Mudar para Barbeiro
+          </button>
+          <button onClick={handleLogin} type="button">
+            Fazer login
+          </button>
+        </div>
+      </div>
+    </MainContainer>
+  );
+};
+export default UserLogin;
